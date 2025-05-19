21969542fee7031e3d6e8997cfc55d452ca54429
Mohamed Azmi <pydevazmi@gmail.com>
2025-05-19 13:39:12 +0300

    feat: Implement initial blog app structure with models, schema, and GraphQL integration

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local_apps
+    "blog",
+    
+    # installed_apps
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
